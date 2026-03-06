dafaf67fb5030424685a4011b73bcc0c5e038744
강경원 <kwkang@ssrinc.co.kr>
2026-03-06 18:26:31 +0900

    🔨 헥사고날 아키텍처 적용 및 QueryDSL 세팅 (#2)
    - Ports & Adapters 구조로 패키지 재구성
    - QueryDSL 의존성 및 설정 추가
    - GlobalExceptionHandler, BusinessException 추가
    - HealthController 이동
    - docker-compose 환경변수 처리

--- src/main/kotlin/com/geuphalttaen/server/adapter/in/web/HealthController.kt
+++ src/main/kotlin/com/geuphalttaen/server/adapter/in/web/HealthController.kt
@@ -1,4 +1,4 @@
-package com.geuphalttaen.server.controller
+package com.geuphalttaen.server.adapter.`in`.web
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
